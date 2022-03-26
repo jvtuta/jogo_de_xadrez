14588ec7e63c97ffdb929be1915a0a9554462176
jvtuta <joaovoliveira299@gmail.com>
2022-03-26 17:48:50 -0300

    ChessException and ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,12 +1,17 @@
 package boardgame;
 
+import boardgame.exceptions.BoardException;
+
 public class Board {
 	
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 	
-	public Board(int rows,  int columns) {
+	public Board(int rows,  int columns)  {
+		if(rows < 1 || columns < 1 ) {
+			throw new BoardException("É necessário que haja pelo menos uma linha e coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,19 +21,14 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-	public Piece piece(int row, int column) {
+	public Piece piece(int row, int column)  {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position is not on the board");
+		}
 		return pieces[row][column];
 	}
 	
@@ -37,7 +37,25 @@ public class Board {
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on the position "+ position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row <= rows && column >= 0 && column <= columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position is not on the board");
+		}
+		return piece(position) != null;
 	}
 }
